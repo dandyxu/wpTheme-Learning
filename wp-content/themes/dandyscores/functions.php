090c5eebdae99ec8521818bc6b6de1368e5f7d1c
dandyxu <dandyjefferson@gmail.com>
2017-09-22 12:45:53 +0100

    Import Google Fonts

--- wp-content/themes/dandyscores/functions.php
+++ wp-content/themes/dandyscores/functions.php
@@ -117,6 +117,9 @@ add_action( 'widgets_init', 'dandyscores_widgets_init' );
  * Enqueue scripts and styles.
  */
 function dandyscores_scripts() {
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style( 'dandyscores-font', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,600,900');
+
 	wp_enqueue_style( 'dandyscores-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'dandyscores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
