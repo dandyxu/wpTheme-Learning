5580ee31465dcf89dfaf8f90265bfc9e2a6506ea
dandyxu <dandyjefferson@gmail.com>
2017-09-22 15:11:47 +0100

    Optional Header Image added

--- wp-content/themes/dandyscores/inc/custom-header.php
+++ wp-content/themes/dandyscores/inc/custom-header.php
@@ -1,72 +1,76 @@
 <?php
 /**
- * Sample implementation of the Custom Header feature
+ * Sample implementation of the Custom Header feature.
  *
  * You can add an optional custom header image to header.php like so ...
  *
-	<?php the_header_image_tag(); ?>
+	<?php if ( get_header_image() ) : ?>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+	</a>
+	<?php endif; // End header image check. ?>
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package Dandyscores
+ * @package Humescores
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses dandyscores_header_style()
+ * @uses humescores_header_style()
  */
-function dandyscores_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'dandyscores_custom_header_args', array(
+function humescores_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'humescores_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => 'dandyscores_header_style',
+		'wp-head-callback'       => 'humescores_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', 'dandyscores_custom_header_setup' );
+add_action( 'after_setup_theme', 'humescores_custom_header_setup' );
 
-if ( ! function_exists( 'dandyscores_header_style' ) ) :
-	/**
-	 * Styles the header image and text displayed on the blog.
-	 *
-	 * @see dandyscores_custom_header_setup().
+if ( ! function_exists( 'humescores_header_style' ) ) :
+/**
+ * Styles the header image and text displayed on the blog.
+ *
+ * @see humescores_custom_header_setup().
+ */
+function humescores_header_style() {
+	$header_text_color = get_header_textcolor();
+
+	/*
+	 * If no custom options for text are set, let's bail.
+	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
 	 */
-	function dandyscores_header_style() {
-		$header_text_color = get_header_textcolor();
+	if ( HEADER_TEXTCOLOR === $header_text_color ) {
+		return;
+	}
 
-		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-		 */
-		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-			return;
-		}
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
+	// If we get this far, we have custom styles. Let's do this.
+	?>
+	<style type="text/css">
+	<?php
 		// Has the text been hidden?
 		if ( ! display_header_text() ) :
-		?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-			}
-		<?php
-			// If the user has set a custom color for the text use that.
-			else :
-		?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
-		</style>
-		<?php
-	}
+	?>
+		.site-title,
+		.site-description {
+			position: absolute;
+			clip: rect(1px, 1px, 1px, 1px);
+		}
+	<?php
+		// If the user has set a custom color for the text use that.
+		else :
+	?>
+		.site-title a,
+		.site-description {
+			color: #<?php echo esc_attr( $header_text_color ); ?>;
+		}
+	<?php endif; ?>
+	</style>
+	<?php
+}
 endif;
