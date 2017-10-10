de11704eb5096782ac8eb2de2607e5dacbdccd67
dandyxu <dandyjefferson@gmail.com>
2017-10-10 21:48:28 +0100

    Customize archive pages - archives.php -> Make archive_title, archive_description full-width - style to class .page-header, .page-title, .archive-description

--- wp-content/themes/dandyscores/archive.php
+++ wp-content/themes/dandyscores/archive.php
@@ -9,18 +9,23 @@
 
 get_header(); ?>
 
+<?php
+    if ( have_posts() ) : ?>
+
+    <header class="page-header">
+		<?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+    </header><!-- .page-header -->
+
+<?php endif; ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 		<?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -35,7 +40,12 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
+			//the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' => __( 'Newer', 'dandyscores' ),
+				'next_text' => __( 'Older', 'dandyscores' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ' , 'dandyscores' ) . '</span>',
+			));
 
 		else :
 
