4c1f27ee76a632e39bc5e74c4806709fc930564e
dandyxu <dandyjefferson@gmail.com>
2017-10-24 11:28:38 +0100

    1301: Customize responsive image outout for all images functions.php  => content_image, header_image, post_thumbnail

--- wp-content/themes/dandyscores/functions.php
+++ wp-content/themes/dandyscores/functions.php
@@ -244,6 +244,76 @@ function dandyscores_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'dandyscores_scripts' );
 
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for content images.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $sizes A source size value for use in a 'sizes' attribute.
+ * @param array  $size  Image size. Accepts an array of width and height
+ *                      values in pixels (in that order).
+ * @return string A source size value for use in a content image 'sizes' attribute.
+ */
+function dandyscores_content_image_sizes_attr( $sizes, $size ) {
+	$width = $size[0];
+
+	if ( 900 <= $width ) {
+		$sizes = '(max-width: 900px) 700px, 900px';
+	}
+
+	if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) {
+		$sizes = ' (min-width: 900px) 600px, 900px';
+	}
+
+	return $sizes;
+}
+add_filter( 'wp_calculate_image_sizes', 'dandyscores_content_image_sizes_attr', 10, 2 );
+
+/**
+ * Filter the `sizes` value in the header image markup.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $html   The HTML image tag markup being filtered.
+ * @param object $header The custom header object returned by 'get_custom_header()'.
+ * @param array  $attr   Array of the attributes for the image tag.
+ * @return string The filtered header image HTML.
+ */
+function dandyscores_header_image_tag( $html, $header, $attr ) {
+	if ( isset( $attr['sizes'] ) ) {
+		$html = str_replace( $attr['sizes'], '100vw', $html );
+	}
+	return $html;
+}
+add_filter( 'get_header_image_tag', 'dandyscores_header_image_tag', 10, 3 );
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param array $attr       Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size       Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function dandyscores_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+	if ( !is_singular() ) {
+		if ( is_active_sidebar ( 'sidebar-1') ) {
+			$attr['sizes'] = '(max-width: 900px) 90vw, 800px';
+		}else {
+		$attr['sizes'] = '(max-wdith: 1000px) 90vw, 1000px';
+		}
+	}
+
+	return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'dandyscores_post_thumbnail_sizes_attr', 10, 3 );
+
+
 /**
  * Implement the Custom Header feature.
  */
