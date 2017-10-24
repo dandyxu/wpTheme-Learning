63870c797d7865782a6147363a717f2022cd69a3
dandyxu <dandyjefferson@gmail.com>
2017-10-24 22:39:01 +0100

    1404: Apply Customizer changes to front-end /inc/custom-header.php /inc/customizer.php

--- wp-content/themes/dandyscores/inc/custom-header.php
+++ wp-content/themes/dandyscores/inc/custom-header.php
@@ -12,65 +12,23 @@
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package Humescores
+ * @package dandyscores
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses humescores_header_style()
+ * @uses dandyscores_header_style()
  */
-function humescores_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'humescores_custom_header_args', array(
+function dandyscores_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'dandyscores_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => 'ffffff',
 		'width'                  => 2000,
 		'height'                 => 850,
 		'flex-height'            => true,
-		'wp-head-callback'       => 'humescores_header_style',
+		'wp-head-callback'       => 'dandyscores_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', 'humescores_custom_header_setup' );
+add_action( 'after_setup_theme', 'dandyscores_custom_header_setup' );
 
-if ( ! function_exists( 'humescores_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog.
- *
- * @see humescores_custom_header_setup().
- */
-function humescores_header_style() {
-	$header_text_color = get_header_textcolor();
-
-	/*
-	 * If no custom options for text are set, let's bail.
-	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
-	 */
-	if ( HEADER_TEXTCOLOR === $header_text_color ) {
-		return;
-	}
-
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif;
--- wp-content/themes/dandyscores/inc/customizer.php
+++ wp-content/themes/dandyscores/inc/customizer.php
@@ -81,3 +81,57 @@ function dandyscores_customize_preview_js() {
 	wp_enqueue_script( 'dandyscores-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'dandyscores_customize_preview_js' );
+
+if ( ! function_exists( 'dandyscores_header_style' ) ) :
+	/**
+	 * Styles the header image and text displayed on the blog.
+	 *
+	 * @see dandyscores_custom_header_setup().
+	 */
+	function dandyscores_header_style() {
+		$header_text_color = get_header_textcolor();
+		$header_bg_color = get_theme_mod( 'theme_bg_color' );
+	
+		/*
+		 * If no custom options for text are set, let's bail.
+		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
+		 */
+		if ( HEADER_TEXTCOLOR != $header_text_color ) {
+	
+			// If we get this far, we have custom styles. Let's do this.
+			?>
+			<style type="text/css">
+			<?php
+				// Has the text been hidden?
+				if ( ! display_header_text() ) :
+			?>
+				.site-title,
+				.site-description {
+					position: absolute;
+					clip: rect(1px, 1px, 1px, 1px);
+				}
+			<?php
+				// If the user has set a custom color for the text use that.
+				else :
+			?>
+				.site-title a,
+				.site-description {
+					color: #<?php echo esc_attr( $header_text_color ); ?>;
+				}
+			<?php endif; ?>
+			</style>
+			<?php
+		}
+
+		if ( '#002254' != $header_bg_color ){ ?>
+			<style type="text/css">
+				.site_header, 
+				.site_footer {
+					background-color: <?php echo esc_attr( $header_bg_color ); ?>;
+				}
+			</style>
+		<?php
+		}
+	}
+	endif;
+	
